e4089b1becf97963d53d49c87744b1584b91452d
Ercan Umut Danışan <ercan.danisan@metu.edu.tr>
2026-04-23 17:55:07 +0300

    Add random banner image selector

---
+++ style/banner.js
@@ -0,0 +1,42 @@
+// Random banner image selector
+// Add new images to this list when you put them in style/banners/
+(function () {
+  var banners = [
+    'nozzle.jpg',
+    'banner2.jpg',
+    'banner3.jpg'
+    // Add more filenames here as you add images to style/banners/
+  ];
+
+  var pick = banners[Math.floor(Math.random() * banners.length)];
+
+  // Figure out path depth: pages in subfolders need "../"
+  // We detect by checking the current URL path
+  var depth = (window.location.pathname.match(/\//g) || []).length - 1;
+  // GitHub Pages adds repo name; use a more robust check
+  var prefix = '';
+  var scripts = document.getElementsByTagName('script');
+  for (var i = 0; i < scripts.length; i++) {
+    var src = scripts[i].src || '';
+    if (src.indexOf('banner.js') !== -1) {
+      // extract the prefix up to /style/banner.js
+      prefix = src.substring(0, src.lastIndexOf('/style/'));
+      break;
+    }
+  }
+
+  function applyBanner() {
+    var logo = document.getElementById('logo');
+    if (logo) {
+      logo.style.backgroundImage = "url('" + prefix + "/style/banners/" + pick + "')";
+      logo.style.backgroundSize = 'cover';
+      logo.style.backgroundPosition = 'center center';
+    }
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', applyBanner);
+  } else {
+    applyBanner();
+  }
+})();
